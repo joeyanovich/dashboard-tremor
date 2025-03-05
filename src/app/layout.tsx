1a1b0c1d19d1e31041ef1428c23648859635749f
joeyanovich <joedison.dias@gmail.com>
2025-03-05 13:50:56 -0300

    feat: create structure of sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "@/components/sidebar-dropdown/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="antialiased">
+      {/* <html lang="pt-br" className="antialiased dark:bg-gray-950"> */}
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <Sidebar />
+        <main className="lg:pl-72">{children}</main>
       </body>
     </html>
   );
